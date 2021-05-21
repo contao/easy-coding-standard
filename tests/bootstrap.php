651838c5ffbb802cf2ce91f2ef70b9e52cbc8417
Leo Feyer <github@contao.org>
2021-05-21 10:14:48 +0200

    Fix the CI chain (see #10)
    Description
    -----------
    
    I'd still love to use symplify/easy-coding-standard-tester (see symplify/symplify#3205) but if that should not work out, this is an easy way to fix the CI chain.
    
    Commits
    -------
    
    ee21ad31 Fix the CI chain

---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../vendor/symplify/easy-coding-standard/vendor/squizlabs/php_codesniffer/autoload.php';
